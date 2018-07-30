15bd80a629ef870297dd41a10a6173787c4cb831
curtain <1158380326@qq.com>
2018-07-30 19:25:10 +0800

    测试carinfo

--- src/test/groovy/org/crygier/graphql/mlshop/service/impl/CarInfoServiceImpl.java
+++ src/test/groovy/org/crygier/graphql/mlshop/service/impl/CarInfoServiceImpl.java
@@ -4,11 +4,13 @@ import org.crygier.graphql.mlshop.model.CarInfo;
 import org.crygier.graphql.mlshop.repo.CarInfoRepository;
 import org.crygier.graphql.mlshop.service.CarInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Curtain
  * @date 2018/7/30 18:25
  */
+@Service
 public class CarInfoServiceImpl implements CarInfoService {
 
     @Autowired
