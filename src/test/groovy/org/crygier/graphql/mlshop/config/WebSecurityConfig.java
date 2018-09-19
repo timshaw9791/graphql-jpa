e009598fe899d187740fea5cd993d32bcd4eab78
curtain <1158380326@qq.com>
2018-09-19 16:22:11 +0800

    正式服环境分支

--- src/test/groovy/org/crygier/graphql/mlshop/config/WebSecurityConfig.java
+++ src/test/groovy/org/crygier/graphql/mlshop/config/WebSecurityConfig.java
@@ -69,7 +69,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/login", "/client/**", "/mlshop/getcode", "/mlshop/verify", "/registeruser", "/forgetpassword", "/sts", "/mlshop/pay", "/mlshop/notify").permitAll()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/**").authenticated()
                 // .antMatchers("/agency/**").hasAnyRole("ADMIN")
                 .and()
                 .formLogin()
@@ -86,7 +86,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         //解决静态资源被拦截的问题
-        web.ignoring().mvcMatchers("/graphql");
         web.ignoring().mvcMatchers("/pay/**");
         web.ignoring().mvcMatchers("/mlshop/pay/**");
     }
